928986179ee9011ca194b7597d62285f9c76bf42
nehavijpaur <69444595+nehavijpaur@users.noreply.github.com>
2024-10-22 19:29:53 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -1,15 +1,7 @@
-provider "azurerm" {
-  features = {}
-}
-
-
 terraform {
-  required_version = ">= 0.14"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.1.0"
+      version = "=3.0.0"
     }
   }
-}
