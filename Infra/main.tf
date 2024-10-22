9038141c521389d6dfd75ae83710443b44d65162
nehavijpaur <69444595+nehavijpaur@users.noreply.github.com>
2024-10-22 13:30:34 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 
   subscription_id   = "b23019ef-882f-400c-bec3-05ad0c529130"
   tenant_id         = "c6d5705d-c3d8-44a2-9454-7f00b90ee67c"
-  client_id         = "c7bcd53a-5ad8-4b01-8fad-6b0d6280e3b0"
+  client_id         = "a882255f-41b9-43e6-85a2-e7495a28db04"
 }
 
 resource "azurerm_resource_group" "infrarg_1" {
