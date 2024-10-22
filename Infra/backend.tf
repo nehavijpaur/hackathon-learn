76df224d745fde19b21ef331a98111a68cc6c0cc
nehavijpaur <69444595+nehavijpaur@users.noreply.github.com>
2024-10-22 19:25:52 +0530

    Create backend.tf

---
+++ Infra/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "backendtf-rg"
+    storage_account_name = "statefilestrg"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    tenant_id            = "c6d5705d-c3d8-44a2-9454-7f00b90ee67c"
+    subscription_id      = "b23019ef-882f-400c-bec3-05ad0c529130"
+  }
+}
