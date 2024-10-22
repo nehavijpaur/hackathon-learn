bf75b369040a4500e7b2bf367707cb5c2e1a13e0
nehavijpaur <69444595+nehavijpaur@users.noreply.github.com>
2024-10-22 19:29:15 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -6,6 +6,6 @@ terraform {
     key                  = "terraform.tfstate"
     tenant_id            = "c6d5705d-c3d8-44a2-9454-7f00b90ee67c"
     subscription_id      = "b23019ef-882f-400c-bec3-05ad0c529130"
-    client-id            = "a882255f-41b9-43e6-85a2-e7495a28db04"
+    client_id            = "a882255f-41b9-43e6-85a2-e7495a28db04"
   }
 }
