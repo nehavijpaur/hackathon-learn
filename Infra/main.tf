4c0a023d7e223e4767caba8708e584be5c562c7b
nehavijpaur <69444595+nehavijpaur@users.noreply.github.com>
2024-10-23 08:56:52 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -18,10 +18,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "b23019ef-882f-400c-bec3-05ad0c529130"
-  client_id       = "a882255f-41b9-43e6-85a2-e7495a28db04"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "c6d5705d-c3d8-44a2-9454-7f00b90ee67c"
+#  subscription_id = "b23019ef-882f-400c-bec3-05ad0c529130"
+#  client_id       = "a882255f-41b9-43e6-85a2-e7495a28db04"
+#  client_secret   = "${var.client_secret}"
+#  tenant_id       = "c6d5705d-c3d8-44a2-9454-7f00b90ee67c"
 }
 
 resource "azurerm_resource_group" "infrarg_1" {
